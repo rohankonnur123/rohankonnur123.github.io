cca309ab78856153360d4172b2dd56fd628b0c6d
Rohan Konnur <rohankonnur@berkeley.edu>
2018-03-18 23:32:34 -0700

    Instruct login

--- js/feedback.js
+++ js/feedback.js
@@ -1,4 +1,4 @@
-var sourcestudentname = document.getElementById("sourcestudentname")
+var sourcestudentname = document.getElementById("studname")
 
 var p1overallScore = document.getElementById("p1overallScore")
 var p1creativityscore = document.getElementById("p1creativityscore")
@@ -20,12 +20,63 @@ var p1additionalscore = document.getElementById("p1additionalscore")
 // var ref = database.ref().child("Project 1")
 // ref.on("value", )
 
-firebasereference = firebase.database().ref().child("Project 1").child(sourcestudentname).child("Justin M").child("Slider 1 Value")
+firebasereference1 = firebase.database().ref().child("Project 1").child("Rohan K").child("Justin M").child("Slider 1 Value")
+firebasereference2 = firebase.database().ref().child("Project 1").child("Rohan K").child("Justin M").child("Slider 3 Value")
+firebasereference3 = firebase.database().ref().child("Project 1").child("Rohan K").child("Justin M").child("Slider 4 Value")
+firebasereference4 = firebase.database().ref().child("Project 1").child("Rohan K").child("Justin M").child("Slider 6 Value")
+firebasereference5 = firebase.database().ref().child("Project 1").child("Rohan K").child("Justin M").child("Slider 7 Value")
+firebasereference6 = firebase.database().ref().child("Project 1").child("Rohan K").child("Justin M").child("Slider 8 Value")
+firebasereference7 = firebase.database().ref().child("Project 1").child("Rohan K").child("Justin M").child("Slider 9 Value")
+firebasereference8 = firebase.database().ref().child("Project 1").child("Rohan K").child("Justin M").child("Slider 11 Value")
+firebasereference9 = firebase.database().ref().child("Project 1").child("Rohan K").child("Justin M").child("Slider 12 Value")
+firebasereference10 = firebase.database().ref().child("Project 1").child("Rohan K").child("Justin M").child("Slider 13 Value")
+firebasereference11 = firebase.database().ref().child("Project 1").child("Rohan K").child("Justin M").child("Slider 14 Value")
+firebasereference12 = firebase.database().ref().child("Project 1").child("Rohan K").child("Justin M").child("Slider 15 Value")
+firebasereference13 = firebase.database().ref().child("Project 1").child("Rohan K").child("Justin M").child("Slider 16 Value")
+firebasereference14 = firebase.database().ref().child("Project 1").child("Rohan K").child("Justin M").child("Slider 17 Value")
 
-firebasereference.on('value', function(databasesnapshot1) {
+firebasereference1.on('value', function(databasesnapshot1) {
 	p1creativityscore.innerText = databasesnapshot1.val();
 });
-
+firebasereference2.on('value', function(databasesnapshot1) {
+	p1cooperationscore.innerText = databasesnapshot1.val();
+});
+firebasereference3.on('value', function(databasesnapshot1) {
+	p1communicationscore.innerText = databasesnapshot1.val();
+});
+firebasereference4.on('value', function(databasesnapshot1) {
+	p1organizationscore.innerText = databasesnapshot1.val();
+});
+firebasereference5.on('value', function(databasesnapshot1) {
+	p1contributionscore.innerText = databasesnapshot1.val();
+});
+firebasereference6.on('value', function(databasesnapshot1) {
+	p1timemanagementscore.innerText = databasesnapshot1.val();
+});
+firebasereference7.on('value', function(databasesnapshot1) {
+	p1problemsolvingscore.innerText = databasesnapshot1.val();
+});
+firebasereference8.on('value', function(databasesnapshot1) {
+	p1reflectionscore.innerText = databasesnapshot1.val();
+});
+firebasereference9.on('value', function(databasesnapshot1) {
+	p1reliablescore.innerText = databasesnapshot1.val();
+});
+firebasereference10.on('value', function(databasesnapshot1) {
+	p1leadershipscore.innerText = databasesnapshot1.val();
+});
+firebasereference11.on('value', function(databasesnapshot1) {
+	p1positivescore.innerText = databasesnapshot1.val();
+});
+firebasereference12.on('value', function(databasesnapshot1) {
+	p1admirescore.innerText = databasesnapshot1.val();
+});
+firebasereference13.on('value', function(databasesnapshot1) {
+	p1improvescore.innerText = databasesnapshot1.val();
+});
+firebasereference14.on('value', function(databasesnapshot1) {
+	p1additionalscore.innerText = databasesnapshot1.val();
+});
 
 
 
--- js/talogin.js
+++ js/talogin.js
@@ -1,6 +1,11 @@
+var iuser = document.getElementById('iuser').value
+var ipass = document.getElementById("ipass").value
+
 function talogin() {
-	if ([value of textbox for username] == TA && [value of textbox for password] == admin) {
-    [go to next page]
-} else {
-    console.log("incorrect password")
-}}
+	if (iuser == "TA" && ipass == "admin") {
+	    window.location.href = "https://rohankonnur123.github.io/dashboard_instructor.html";
+	} else {
+		window.location.href = "https://rohankonnur123.github.io/";
+	    console.log("incorrect password")
+	}
+}
